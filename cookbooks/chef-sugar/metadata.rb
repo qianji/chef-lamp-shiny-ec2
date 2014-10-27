4e6c54e54aa83c776f570186f07a5f46fa09363d
qianji <qianji.zheng@gmail.com>
2014-10-27 10:50:17 -0500

    Import chef-sugar version 2.4.1

--- cookbooks/chef-sugar/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             "chef-sugar"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Distribute and enable Chef Exception and Report handlers"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.4.1"
